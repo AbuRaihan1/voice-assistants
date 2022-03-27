8e517d200ab8be89f0d9f5d6dc700bd378e743f7
Raihan Ahmed <raihanahmed01973@gmail.com>
2022-03-27 13:14:19 +0600

    a

--- robot.js
+++ robot.js
@@ -12,14 +12,23 @@ function read(text) {
   speech.text = text;
   if (text == "hello") {
     speech.text = "hi. how are you?";
-  } else if (text == "who is made you ?") {
-    (" Raihan Ahmed Make me");
-  } else if (text == "who is shiblu's gf ?") {
-    (" haha. its a weard question. if i said this, i think shiblu will heart me. but, i don't have to do anything. Raihan called me to say this.so, i have to say this. Shiblu's gf name is Rima");
-  } else if (text == "i am fine . and you?") {
+  }
+  if (text== 'hi'){
+    speech.text ="hello. How are you?"
+  } 
+  
+  else if (text == "who is made you") {
+    (" Raihan Ahmed Makes me");
+  } else if (text == "who is shiblu gf") {
+    (" haha. its a weard question. if i said this, i think shiblu will heart me. but, i don't have to do anything. Raihan Ahmed called me to say this. so, i have to say this Name. Shiblu's gf name is Rima");
+  } else if (text == "i am fine and you") {
     speech.text = "I'm Also fine.";
-  } else {
-    ("i am not a dynamic robot. i have a simple program. please ask me another question");
+  } 
+  else if (text == "what is your name"){
+    speech.text = "my name not defined yet. my name will defined jalajontrona"
+  }
+    else {
+    speech.text="please try again"
   }
   document.getElementById("ai_voice").innerHTML = speech.text;
   window.speechSynthesis.speak(speech);
