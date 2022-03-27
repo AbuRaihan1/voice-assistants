085d2cc4a4a79fcd14fb38973ff1af26c53a5cc8
Raihan Ahmed <raihanahmed01973@gmail.com>
2022-03-27 13:05:45 +0600

    ra

--- robot.js
+++ robot.js
@@ -1,29 +1,26 @@
+let recognition = new webkitSpeechRecognition();
 
-let recognition = new webkitSpeechRecognition()
-console.log(recognition)
+recognition.onresult = function (event) {
+  var text = event.results[0][0].transcript;
+  var user_voice = (document.getElementById("user_voice").innerHTML = text);
 
-recognition.onresult = function(event){
-  var text = event.onresults[0][0].transcript;
-  var user_voice = document.getElementById('user_voice').innerHTML = text;
+  read(text);
+};
 
-  read(text)
-}
-
-function read(text){
+function read(text) {
   var speech = new SpeechSynthesisUtterance();
   speech.text = text;
-  if(text == 'hello'){
-    speech.text = 'hi. how are you?';
+  if (text == "hello") {
+    speech.text = "hi. how are you?";
+  } else if (text == "who is made you ?") {
+    (" Raihan Ahmed Make me");
+  } else if (text == "who is shiblu's gf ?") {
+    (" haha. its a weard question. if i said this, i think shiblu will heart me. but, i don't have to do anything. Raihan called me to say this.so, i have to say this. Shiblu's gf name is Rima");
+  } else if (text == "i am fine . and you?") {
+    speech.text = "I'm Also fine.";
+  } else {
+    ("i am not a dynamic robot. i have a simple program. please ask me another question");
   }
-  else if(text == 'who is made you ?'){
-    ' Raihan Ahmed Make me'
-  }
-  else if(text = "who is shiblu's gf ?"){
-    "Rima is Shiblu's gf"
-  }
-  else{
-    'i am not a dynamic robot. i have a simple program. please ask me another question'
-  }
-  document.getElementById('ai_voice').innerHTML = speech.text;
+  document.getElementById("ai_voice").innerHTML = speech.text;
   window.speechSynthesis.speak(speech);
 }
